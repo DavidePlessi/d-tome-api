0adb92bd21c5ff2e83fc846857887b44401e3046
Davide <dp@art4art.it>
2023-06-14 00:09:37 +0200

    First commit

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const NEO4J_URI = process.env.NEO4J_URI;
+export const NEO4J_USER = process.env.NEO4J_USER;
+export const NEO4J_PASSWORD = process.env.NEO4J_PASSWORD;
---
+++ src/services/neo4jService.ts
@@ -0,0 +1,58 @@
+import neo4j, {Driver} from 'neo4j-driver';
+import {NEO4J_PASSWORD, NEO4J_URI, NEO4J_USER} from "../config";
+
+class Neo4jService {
+  private driver: Driver;
+  constructor (
+    uri: string = NEO4J_URI,
+    user: string = NEO4J_USER,
+    password: string = NEO4J_PASSWORD
+  ) {
+    this.driver = neo4j.driver(
+      uri,
+      neo4j.auth.basic(
+        user,
+        password
+      )
+    );
+  }
+
+  async run(query, params) {
+    const session = this.driver.session();
+
+    try {
+      return await session.run(query, params);
+    } finally {
+      await session.close();
+    }
+  }
+
+  async read(query, params) {
+    const session = this.driver.session();
+
+    try {
+      return await session.executeRead(
+          tx => tx.run(query, params)
+      );
+    } finally {
+      await session.close();
+    }
+  }
+
+  async write(query, params) {
+    const session = this.driver.session();
+
+    try {
+      return await session.executeWrite(tx => tx.run(query, params));
+    } finally {
+      await session.close();
+    }
+  }
+
+  async close() {
+    await this.driver.close();
+  }
+}
+
+const neo4jService = new Neo4jService();
+export default neo4jService;
